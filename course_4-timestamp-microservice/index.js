4df85dba7e194dc7b439363ab6d88caaca707f6b
Kevin Aragon <aragoncodes@gmail.com>
2023-03-08 16:41:38 +0100

    feat: return 'now' date if no query parameter is specified

--- course_4-timestamp-microservice/index.js
+++ course_4-timestamp-microservice/index.js
@@ -14,19 +14,28 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-app.use('', (req) => {
+app.use('', (req, _, next) => {
   const { path, params, body } = req;
   console.log({ path, params, body });
+
+  next();
 });
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
+app.get('/', function (_, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/:date?', function (req, res) {
+  const dateParam = req.params.date;
+
+  let date;
+
+  if (!dateParam) {
+    date = new Date();
+  }
+
+  res.json({ utc: date.toUTCString(), unix: date.getTime() });
 });
 
 // listen for requests :)
