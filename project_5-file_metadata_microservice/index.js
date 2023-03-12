c46e19c8f6d50bc3df468324f2c4d9f2f27d7406
Kevin Aragon <aragoncodes@gmail.com>
2023-03-12 12:14:21 +0100

    feat: analyze uploaded file on POST /api/fileanalyse

--- project_5-file_metadata_microservice/index.js
+++ project_5-file_metadata_microservice/index.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 
-var app = express();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +14,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname: name, mimetype: type, size } = req.file;
 
-
+  res.json({ name, type, size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
